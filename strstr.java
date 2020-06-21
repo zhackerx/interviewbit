c30af69c2f58dc3460896315de67b343af3bdab5
zhackerx <43398071+zhackerx@users.noreply.github.com>
2020-06-21 15:51:18 +0530

    Create strstr.java

---
+++ strstr.java
@@ -0,0 +1,7 @@
+//implement strstr
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int strStr(final String A, final String B) {
+        return A.indexOf(B)>-1?A.indexOf(B):-1;
+    }
+}
