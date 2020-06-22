5258f56169215da50181e2a6f5c0aa37cf82f6c3
zhackerx <43398071+zhackerx@users.noreply.github.com>
2020-06-22 16:23:43 +0530

    more

---
+++ count_bits.java
@@ -0,0 +1,23 @@
+//count sum of bits
+public class Solution {
+    public int cntBits(ArrayList<Integer> A) {
+         long res = 0;
+        long mod = 1000000007L;
+        for(int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+            long ones = 0;
+            long zeros = 0;
+            for(int num : A) {
+                if((mask & num) != 0) {
+                    ones++;
+                }
+                else {
+                    zeros++;
+                }
+            }
+            res += (ones*zeros*2L % mod);
+        }
+        
+        return (int)(res%mod);
+    }
+}
---
+++ remove_ele.java
@@ -0,0 +1,22 @@
+//remove element from arrray
+public class Solution {
+    public int removeElement(ArrayList<Integer> a, int b) {
+        if(a.size()==0)
+        return 0;
+         int i = 0;
+	    int j = 0;
+	    
+	    while (i<a.size()) {
+	        if (a.get(i) != b) {
+	            if (i != j) {
+	                a.set(j, a.get(i));
+	                a.set(i, b);
+	            }
+	            j++;
+	        }
+	        i++;
+	    }
+	    
+	    return j;
+	}
+}
